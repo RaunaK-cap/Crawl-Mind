e3f41690e483d73dfd08e29010cb433131e961d7
Raunak <raunakkumar6437@gmail.com>
2025-09-20 22:25:50 +0530

    added schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,21 +3,28 @@ import { v} from "convex/values"
 
 export default  defineSchema({
 
-    UserSchema:defineTable({
+    UserSchema:defineTable({                // User data data for to stor user specific data 
         UserID:v.string(),
         Email:v.string(),
-        Firstname:v.string(),
-        Lastname:v.string(),
+        Name:v.string(),
         CreatedAt:v.number()
-    }),
+    }).index("by_UserId" , ["UserID"]),       
+
 
     Embedding_vectors : defineTable({
+        
+        UserID:v.string(),
+        Name:v.string(),
         Text_Chunk: v.string(),             //text chunks 
         Vectors:v.array(v.float64()),       // vector embeddings of the text chunks 
         source:v.string(),                  // urls ,
         CreatedAt:v.number()                //created time 
-    }).vectorIndex("by_embedding" , {
-        vectorField:"Vectors",              // Give  your stored vector access
-        dimensions:1536                     // Check the open AI embedding models dimensions 
     })
+    .vectorIndex("by_embedding" , {          // For Vector indexing/quering 
+        vectorField:"Vectors",              // Give  your stored vector access
+        dimensions:1536,                    // Check Open AI embeddeing model dimensions 
+        filterFields:["UserID"]             //For user specific Filteration
+                                             
+    })
+    .index("by_UserID" , [ "UserID"])      // For Normal indexing/quering 
 })
